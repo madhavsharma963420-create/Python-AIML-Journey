0b2a0e674763b0ef2e956166015b083792f89fd3
madhav9634 <madhavsharma963420@gmail.com>
2026-02-17 23:22:16 +0530

    done

---
+++ 01_Python_Fundamentals/loops/loop_control.py
@@ -0,0 +1,13 @@
+# Break Example
+
+for i in range(1, 10):
+    if i == 5:
+        break
+    print(i)
+
+# Continue Example
+
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print(i)
